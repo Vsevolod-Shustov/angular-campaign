ef2000358148f9330d9318e3c2e0bfd72a655637
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2014-10-14 12:36:10 +0400

    one hex default map

--- js/services.js
+++ js/services.js
@@ -3,7 +3,13 @@ var ucServices = angular.module('ucServices', []);
 ucServices.service('LocalStorageService', [ '$rootScope', function(){
   var service = {
     load: function(key, value){
+    if(localStorage[key]){
+      console.log(key + ' loaded');
       return JSON.parse(localStorage[key]);
+    } else {
+      console.log(key + ' not found. Have one hex map instead.');
+      return {'0 0':{'terrain':'plains','x':0,'y':0}};
+    }
     },
     save: function(key, value){
       localStorage[key] = JSON.stringify(value);
