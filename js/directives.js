ca3e37130b914c9369545ca874de2b76bd89d315
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2015-08-23 12:14:49 +0300

    svg hexagons

--- js/directives.js
+++ js/directives.js
@@ -15,10 +15,19 @@ ucDirectives.directive('mapDrawer', function(){
     //template: "<svg width='640' height='480'></svg>",
     link: function(scope, elem, attrs){
       console.log(scope.globalMap);
-      svgWidth = window.innerWidth;
-      svgHeight = window.innerHeight;
-      hexWidth = 150;
-      hexHeight = 2*hexWidth/Math.sqrt(3);
+      var svgWidth = window.innerWidth;
+      var svgHeight = window.innerHeight;
+      var hexWidth = 150;
+      var hexHeight = 2*hexWidth/Math.sqrt(3);
+      var hexSideLength = hexHeight*Math.sin(Math.PI/6);
+      var hexagon = [
+        {"x":hexWidth/2, "y":0},
+        {"x":hexWidth, "y":(hexHeight-hexSideLength)/2},
+        {"x":hexWidth, "y":hexHeight/2+(hexHeight-hexSideLength)/2},
+        {"x":hexWidth/2, "y":hexHeight},
+        {"x":0, "y":hexHeight/2+(hexHeight-hexSideLength)/2},
+        {"x":0, "y":(hexHeight-hexSideLength)/2}
+      ];
       var svg = d3.select('#map')
         .append('svg')
         .attr('width', svgWidth)
@@ -31,12 +40,21 @@ ucDirectives.directive('mapDrawer', function(){
           .data(newMapArr)
           .enter()
           .append("g")
+          .classed('hex', true)
           .attr("transform", function(d, i) { return "translate(" + i * (hexWidth + 1) + ",0)"; });
-        
-        hex.append('rect')
-          .attr('width', hexWidth)
-          .attr('height', hexHeight)
-          .attr('fill', '#fed');
+
+        hex.append('polygon')
+          .attr('points', function(){
+            var hexagonString="";
+            for(i=0;i<hexagon.length;i++){
+              hexagonString += d3.round(hexagon[i].x);
+              hexagonString += ',';
+              hexagonString += d3.round(hexagon[i].y);
+              hexagonString += ' ';
+            };
+            hexagonString = hexagonString.substr(0, hexagonString.length - 1);
+            return hexagonString;
+          });
         
         hex.append('text')
           .attr("x", 10)
